9e11c41819eed4a14af50e0df2bca4cce8544cdf
ChickenProNut <88220962+ChickenProNut@users.noreply.github.com>
2021-12-05 20:08:38 +0800

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1061,6 +1061,10 @@ class PlayState extends MusicBeatState
 				case 'senpai' | 'roses' | 'thorns':
 					if(daSong == 'roses') FlxG.sound.play(Paths.sound('ANGRY'));
 					schoolIntro(doof);
+
+                                case 'The Protagonist' | 'Insane Combat' | 'Depredation':
+                                        startDialogue(tension-in-nevada);
+
 				default:
 					if (dialogueJson == null)
 						startCountdown();
